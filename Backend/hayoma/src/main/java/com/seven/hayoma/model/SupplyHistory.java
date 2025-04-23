524f0532edd8bc9c3b4eab2737ffe210481d23e5
it23190016 <140576176+it23190016@users.noreply.github.com>
2025-04-23 09:25:28 +0530

    Update supply history backend

--- Backend/hayoma/src/main/java/com/seven/hayoma/model/SupplyHistory.java
+++ Backend/hayoma/src/main/java/com/seven/hayoma/model/SupplyHistory.java
@@ -15,7 +15,11 @@ public class SupplyHistory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    private String supplyId;
+    private String productName;
+    private double quantity;
+    private String unit;
+    private LocalDate date;
 
     @Enumerated(EnumType.STRING)
     private SupplyStatus status;
